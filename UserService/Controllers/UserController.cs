c48268eee404e581236f567ab1fd4fa578aaaead
Arunarathod <arunarathod312@gmail.com>
2026-04-23 02:13:37 +0530

    initial backend api

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            return Ok(new { message = $"User Created: {user.Name} - {user.Email}" });
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
