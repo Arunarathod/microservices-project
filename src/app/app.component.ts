93f0b19e8ab6dfabc9df8eb6791faa976efed740
Arunarathod <arunarathod312@gmail.com>
2026-05-07 01:39:07 +0530

    saving local changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'user-ui';
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    debugger;
+    const data = {
+      name: this.name,
+      email: this.email
+    };
+
+    this.http.post('http://localhost:5000/api/user', data)
+    // this.http.post('https://localhost:7016/api/user', data) 
+      .subscribe({
+        next: (res: any) => {
+          this.message = res.message;
+        },
+        error: (err) => {
+          this.message = "Error calling API";
+          console.log(err);
+        }
+      });
+  }
 }
